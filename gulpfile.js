4c784a60f99109f370ba26a792616997b490d13e
codeapps <codeapps@aol.com>
2016-05-28 14:45:25 -0500

    Fixing Node-Inspector

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,45 @@
 const gulp = require('gulp');
-const less = require('gulp-less');
-const pug = require('gulp-pug');
-const bower = require('gulp-bower');
-const imagemin = require('gulp-imagemin');
-const cleancss = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const nodemon = require('gulp-nodemon');
-const inspector = require('gulp-node-inspector');
 const sync = require('browser-sync');
 const colors = require('colors');
 const deploy = "public";
 
 gulp.task('pug', function () {
+  const pug = require('gulp-pug');
   return gulp.src('views/*.pug')
     .pipe(pug())
     .pipe(gulp.dest(deploy))
 });
 
 gulp.task('less', function () {
+  const less = require('gulp-less');
   return gulp.src('views/less/*.less')
     .pipe(less())
     .pipe(gulp.dest('views/css'))
 });
 
 gulp.task('minify', function() {
+  const cleancss = require('gulp-clean-css');
   return gulp.src('views/css/*.css')
     .pipe(cleancss())
     .pipe(gulp.dest('views/css'))
 });
 
 gulp.task('rename', function () {
+  const rename = require('gulp-rename');
   return gulp.src('views/css/*.css')
     .pipe(rename({extname: ".min.css"}))
     .pipe(gulp.dest(deploy + '/css'))
 });
 
 gulp.task('img', function () {
+  const imagemin = require('gulp-imagemin');
   return gulp.src('views/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest(deploy + '/img'))
 });
 
 gulp.task('bower', function() {
+  const bower = require('gulp-bower');
   return bower('bower_modules/')
     .pipe(gulp.dest(deploy))
 });
@@ -51,6 +49,7 @@ gulp.task('build', ['pug', 'less', 'minify', 'rename', 'bower', 'img'], function
 });
 
 gulp.task('nodemon', function (cb) {
+  const nodemon = require('gulp-nodemon');
   var called = false;
   return nodemon({
     script: './bin/www',
@@ -73,7 +72,6 @@ gulp.task('sync', ['nodemon'], function() {
     ui: false,
     online: false
   });
-
   gulp.watch('views/*.pug', ['pug']);
   gulp.watch('views/pug/*.pug', ['pug']);
   gulp.watch('views/img/*', ['img']);
@@ -89,26 +87,23 @@ gulp.task('start', ['build', 'sync'], function() {
   console.log("Start Successful!".green);
 });
 
-gulp.task('inspect', function () {
-  inspector({
-    debugPort: 5858,
-    webHost: 'localhost:4000',
-    webPort: 8080,
-    saveLiveEdit: true,
-    preload: false,
-    inject: true,
-    stackTraceLimit: 50,
-    sslKey: '',
-    sslCert: ''
-  });
-  sync.create().init({
-    proxy: "127.0.0.1:8080/?port=5858",
-    port: 8080,
-    ui: false,
-    online: false
-  });
+gulp.task('inspect', function() {
+  const inspector = require('gulp-node-inspector');
+  gulp.src([])
+    .pipe(inspector({
+      debugPort: 5858,
+      webHost: 'localhost:4000',
+      webPort: 8080,
+      saveLiveEdit: false,
+      preload: true,
+      inject: true,
+      hidden: [],
+      stackTraceLimit: 50,
+      sslKey: '',
+      sslCert: ''
+    }));
 });
 
-gulp.task('debug', ['inspect','sync'], function() {
+gulp.task('debug', ['inspect', 'sync'], function() {
   console.log("Debugging!".red);
 });
