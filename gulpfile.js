19f105b05a66737a65eface261df7709f42415bb
codeapps <codeapps@aol.com>
2016-06-13 04:37:27 -0500

    Change Const to Var

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp');
-var sync = require('browser-sync');
-var colors = require('colors');
-var deploy = 'public';
-var browser = 'google chrome';
+/*jshint esnext: true */
+const gulp = require('gulp');
+const sync = require('browser-sync');
+const colors = require('colors');
+const deploy = 'public';
+const browser = 'google chrome';
 
 gulp.task('tape', function () {
-  var tape = require('gulp-tape');
-  var tapColorize = require('tap-colorize');
+  const tape = require('gulp-tape');
+  const tapColorize = require('tap-colorize');
   return gulp.src('test/*.js')
     .pipe(tape({
       reporter: tapColorize()
@@ -14,56 +15,56 @@ gulp.task('tape', function () {
 });
 
 gulp.task('pug', function () {
-  var pug = require('gulp-pug');
+  const pug = require('gulp-pug');
   return gulp.src('views/pug/*.pug')
     .pipe(pug())
     .pipe(gulp.dest(deploy));
 });
 
 gulp.task('riot', function () {
-  var riot = require('gulp-riot');
+  const riot = require('gulp-riot');
   gulp.src('views/tag/*.tag')
       .pipe(riot())
       .pipe(gulp.dest(deploy));
 });
 
 gulp.task("babel", function () {
-  var babel = require("gulp-babel");
+  const babel = require("gulp-babel");
   return gulp.src("app.js")
     .pipe(babel())
     .pipe(gulp.dest(deploy));
 });
 
 gulp.task('less', function () {
-  var less = require('gulp-less');
+  const less = require('gulp-less');
   return gulp.src('views/less/*.less')
     .pipe(less())
     .pipe(gulp.dest('views/css'));
 });
 
 gulp.task('img', function () {
-  var imagemin = require('gulp-imagemin');
+  const imagemin = require('gulp-imagemin');
   return gulp.src('views/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest(deploy + '/img'));
 });
 
 gulp.task('minify', function () {
-  var cleancss = require('gulp-clean-css');
+  const cleancss = require('gulp-clean-css');
   return gulp.src('views/css/*.css')
     .pipe(cleancss())
     .pipe(gulp.dest('views/css'));
 });
 
 gulp.task('rename', function () {
-  var rename = require('gulp-rename');
+  const rename = require('gulp-rename');
   return gulp.src('views/css/*.css')
     .pipe(rename({extname: '.min.css'}))
     .pipe(gulp.dest(deploy + '/css'));
 });
 
 gulp.task('bower', function () {
-  var bower = require('gulp-bower');
+  const bower = require('gulp-bower');
   return bower('bower_modules/')
     .pipe(gulp.dest(deploy));
 });
@@ -77,7 +78,7 @@ gulp.task('deploy', ['build', 'minify', 'img', 'bower'], function () {
 });
 
 gulp.task('open', function () {
-  var open = require('gulp-open');
+  const open = require('gulp-open');
   return gulp.src(['./'])
     .pipe(open({
       uri: 'http://localhost:8080/?port=5858',
@@ -86,7 +87,7 @@ gulp.task('open', function () {
 });
 
 gulp.task('inspector', ['sync'], function () {
-  var inspector = require('gulp-node-inspector');
+  const inspector = require('gulp-node-inspector');
   gulp.src([])
     .pipe(inspector({
     saveLiveEdit: true,
@@ -117,7 +118,7 @@ gulp.task('watch', function () {
 });
 
 gulp.task('nodemon', function (cb) {
-  var nodemon = require('gulp-nodemon');
+  const nodemon = require('gulp-nodemon');
   var run = false;
   nodemon({
     script: './bin/www',
@@ -143,7 +144,7 @@ gulp.task('nodemon', function (cb) {
 });
 
 gulp.task('karma', function (done) {
-  var Server = require('karma').Server;
+  const Server = require('karma').Server;
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
@@ -151,7 +152,7 @@ gulp.task('karma', function (done) {
 });
 
 gulp.task('shell', function () {
-  var shell = require('gulp-shell');
+  const shell = require('gulp-shell');
   return gulp.src('*.js', {read: false})
     .pipe(shell([
       'echo <%= f(file.path) %>',
