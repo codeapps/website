bfe3b73c471dd058f9ce6dcfc93105acfabfddb4
codeapps <codeapps@aol.com>
2017-02-20 14:03:25 -0500

    Delete All. Change to Yarn

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var controller = express.Router();
-
-/* GET home page. */
-controller.get('/', function(req, res, next) {
-  res.render('index');
-});
-
-module.exports = controller;
--- controllers/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var controller = express.Router();
-
-/* GET users listing. */
-controller.get('/', function(req, res, next) {
-  res.send('Felipe');
-});
-
-module.exports = controller;
